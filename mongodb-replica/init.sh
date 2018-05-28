bb55642d386d15f008008fcd188932240c4809a4
Alex Sanak <sanak.alex@gmail.com>
2018-05-29 02:08:10 +0300

    one more try

--- mongodb-replica/init.sh
+++ mongodb-replica/init.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 for rs in mongo-primary mongo-secondary2 mongo-secondary1;do
-  mongo --host $rs --eval 'db'
+  mongo --host $rs iptstud --eval 'db'
   if [ $? -ne 0 ]; then
     exit 1
   fi
 done
 
 # Connect to rs1 and configure replica set if not done
-status=$(mongo --host mongo-primary --quiet --eval 'rs.status().members.length')
+status=$(mongo --host mongo-primary iptstud --quiet --eval 'rs.status().members.length')
 if [ $? -ne 0 ]; then
   # Replicaset not yet configured
-  mongo --host rs1 --eval 'rs.initiate({ "_id": "mongodb-replica", "version": 1, "members" : [ {"_id": 1, "host": "mongo-primary:27017"}, {"_id": 2, "host": "mongo-secondary2:27017"}, {"_id": 3, "host": "mongo-secondary1:27017"} ] })';
+  mongo --host rs1 iptstud --eval 'rs.initiate({ "_id": "mongodb-replica", "version": 1, "members" : [ {"_id": 1, "host": "mongo-primary:27017"}, {"_id": 2, "host": "mongo-secondary2:27017"}, {"_id": 3, "host": "mongo-secondary1:27017"} ] })';
 fi
 
 export INIT_DUMP='bookcase-service-dump.js'
