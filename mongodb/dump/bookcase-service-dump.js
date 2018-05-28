afab13724d8f64945f9f5cd095b46a155a933260
Alex Sanak <sanak.alex@gmail.com>
2018-05-28 17:00:31 +0300

    mongo js edited

--- mongodb/dump/bookcase-service-dump.js
+++ mongodb/dump/bookcase-service-dump.js
@@ -5,12 +5,7 @@
 print('dump start');
 
 iptstud.books.update(
-    { "_id": "demo" },
-    {
-    "_id": "demo",
-    "lastSeen": new Date(),
-    "note": "demo note",
-    "books": [
+    [
         {
             "id": 1,
             "title": "Awesome book",
@@ -42,7 +37,7 @@ iptstud.books.update(
             "orderedCount": 0
         },
         {
-            "id": 4
+            "id": 4,
             "title": "Harry Potter",
             "shortDescription": "about Harry Potter",
             "author": "J. K. Rowling.",
@@ -52,7 +47,6 @@ iptstud.books.update(
             "orderedCount": 0
         }
     ]
-    { upsert: true }
 );
 
 print('dump complete');
