c001a455b7e86f46d24bcdcfddb07f3ed0bbc4ea
Alex Sanak <sanak.alex@gmail.com>
2018-05-31 00:26:09 +0300

    init.sh configured

--- mongodb-replica/init.sh
+++ mongodb-replica/init.sh
@@ -1,8 +1,6 @@
 # Make sure 3 replicas available
-hosts="mongo-primary mongo-secondary2 mongo-secondary1"
-for rs in ${hosts}
-do
-  mongo --host ${rs} --eval 'db'
+for rs in mongo-primary mongo-secondary2 mongo-secondary1;do
+  mongo --host $rs --eval 'db'
   if [ $? -ne 0 ]; then
     exit 1
   fi
@@ -12,5 +10,5 @@ done
 status=$(mongo --host mongo-primary --quiet --eval 'rs.status().members.length')
 if [ $? -ne 0 ]; then
   # Replicaset not yet configured
-  mongo --host rs1 --eval 'rs.initiate({ "_id": "mongodb-replica", "version": 1, "members" : [ {"_id": 1, "host": "mongo-primary:27017"}, {"_id": 2, "host": "mongo-secondary2:27017"}, {"_id": 3, "host": "mongo-secondary1:27017"} ] })';
+  mongo --host mongo-primary --eval 'rs.initiate({ _id: "mongodb-replica", version: 1, members: [ { _id: 0, host : "mongo-primary:27017" }, { _id: 1, host : "mongo-secondary2:27017" }, { _id: 2, host : "mongo-secondary1:27017" } ] })';
 fi
